384532d068af93c7f09eaffae65b58390de970ec
Vicente Pinto <vicente.pinto@freiheit.com>
2024-10-17 18:41:10 +0100

    Update example

--- examples/iaas/example_setup.py
+++ examples/iaas/example_setup.py
@@ -25,6 +25,7 @@ from stackit.resourcemanager.models.member import Member
 
 project_id = os.getenv("PROJECT_ID")
 organization_id = os.getenv("ORGANIZATION_ID")
+image_id = os.getenv("IMAGE_ID")
 
 # Create a new API client, that uses default authentication and configuration
 config = Configuration()
@@ -32,33 +33,31 @@ client = DefaultApi(config)
 alpha_client = AlphaDefaultApi(config)
 resource_client = ResourceDefaultApi(config)
 
-admin_vps_routes = ["147.204.0.0/16", "100.64.0.0/10", "8.8.8.8/32"]
-
 create_network_area_payload = CreateNetworkAreaPayload(
-    name="demo-network-area",
+    name="network-area",
     addressFamily=CreateAreaAddressFamily(
         ipv4=CreateAreaIPv4(
             networkRanges=[
-                NetworkRange(prefix="10.0.0.0/16"),
+                NetworkRange(prefix="192.0.2.0/24"),
             ],
-            transferNetwork="192.160.0.0/24",
-            default_nameservers=["9.9.9.9"],
+            transferNetwork="192.0.2.0/24",
+            default_nameservers=["192.0.2.0"],
         )
     ),
 )
-demo_network_area = client.create_network_area(organization_id, create_network_area_payload)
+network_area = client.create_network_area(organization_id, create_network_area_payload)
 
 
 project = resource_client.create_project(
     CreateProjectPayload(
-        name="demo_network_area",
+        name="network_area",
         container_parent_id=organization_id,
-        labels={"networkArea": demo_network_area.area_id},
+        labels={"networkArea": network_area.area_id},
         members=[Member(role="owner", subject="example@stackit.cloud")],
     )
 )
 project_id = project.project_id
-demo_network_area_area_id = demo_network_area.area_id
+network_area_area_id = network_area.area_id
 
 default_network_creation_response = client.create_network(
     project_id,
@@ -77,34 +76,34 @@ security_group1 = alpha_client.create_security_group(
 )
 security_group1_groupid = security_group1.id
 
-gateway_a_network = client.create_network(
+network = client.create_network(
     project_id,
     CreateNetworkPayload(
-        name="gateway_a_network",
+        name="network",
     ),
 )
 
-gateway_a_nic1 = alpha_client.create_nic(
+nic1 = alpha_client.create_nic(
     project_id,
-    gateway_a_network.network_id,
+    network.network_id,
     CreateNICPayload(
-        allowed_addresses=[x for x in admin_vps_routes],
+        allowed_addresses=["192.0.2.0/24"],
         security_groups=[security_group1_groupid],
     ),
 )
 
-gateway_a_public_ip = alpha_client.create_public_ip(
-    project_id, CreatePublicIPPayload(network_interface=gateway_a_nic1.id)
+public_ip = alpha_client.create_public_ip(
+    project_id, CreatePublicIPPayload(network_interface=nic1.id)
 )
 
-gateway_a = alpha_client.create_server(
+server = alpha_client.create_server(
     project_id,
     CreateServerPayload(
-        name="gateway-a",
+        name="example-server",
         boot_volume=BootVolume(
             size=64,
             source=BootVolumeSource(
-                id="db10714b-5d17-4a88-83ea-cb499530acc8",
+                id=image_id,
                 type="image",
             ),
         ),
@@ -115,50 +114,4 @@ gateway_a = alpha_client.create_server(
     ),
 )
 time.sleep(180)  # wait for server to be created, temporary sleep until waiter methods are implemented
-alpha_client.add_nicto_server(project_id, gateway_a.id, gateway_a_nic1.id)
-
-
-admin_area_routes = client.create_network_area_route(
-    organization_id,
-    demo_network_area_area_id,
-    CreateNetworkAreaRoutePayload(ipv4=[Route(prefix=x, nexthop=gateway_a_nic1.ipv4) for x in admin_vps_routes]),
-)
-
-
-tenant_network = client.create_network(
-    project_id,
-    CreateNetworkPayload(
-        name="tenant_network",
-    ),
-)
-
-tenant_nic = alpha_client.create_nic(
-    project_id,
-    tenant_network.network_id,
-    CreateNICPayload(
-        security_groups=[security_group1_groupid],
-    ),
-)
-
-tenant_public_ip = alpha_client.create_public_ip(project_id, CreatePublicIPPayload(network_interface=tenant_nic.id))
-
-
-tenant = alpha_client.create_server(
-    project_id,
-    CreateServerPayload(
-        name="tenant",
-        boot_volume=BootVolume(
-            size=64,
-            source=BootVolumeSource(
-                id="db10714b-5d17-4a88-83ea-cb499530acc8",
-                type="image",
-            ),
-        ),
-        keypair_name="key",
-        labels={"key": "value"},
-        machine_type="t1.2",
-        availability_zone="eu01-1",
-    ),
-)
-time.sleep(180)  # wait for server to be created
-alpha_client.add_nicto_server(project_id, tenant.id, tenant_nic.id)
+alpha_client.add_nicto_server(project_id, server.id, nic1.id)
