f2af4a94f013c29b1479403b3aea25717b4959e1
Melvin Klein <melvin.klein@stackit.cloud>
2024-10-30 16:47:01 +0100

    adds waiting for dns

---
+++ services/dns/src/stackit/dns/wait.py
@@ -0,0 +1,253 @@
+from enum import Enum
+from typing import Any, Callable, Tuple, Union
+from stackit.dns.api.default_api import DefaultApi
+from stackit.dns.models.record_set_response import RecordSetResponse
+from stackit.dns.models.zone_response import ZoneResponse
+from stackit.dns.exceptions import ApiException
+from stackit.core.wait import Wait
+
+
+class _States(str, Enum):
+    CreateSuccess = "CREATE_SUCCEEDED"
+    CreateFail = "CREATE_FAILED"
+    UpdateSuccess = "UPDATE_SUCCEEDED"
+    UpdateFail = "UPDATE_FAILED"
+    DeleteSuccess = "DELETE_SUCCEEDED"
+    DeleteFail = "DELETE_FAILED"
+
+
+def wait_for_create_zone(
+    api_client: DefaultApi,
+    project_id: str,
+    zone_id: str,
+    sleep_before_wait: int = 0,
+    throttle: int = 5,
+    timeout: int = 30,
+    temp_error_retry_limit: int = 5,
+) -> ZoneResponse:
+
+    def get_zone_execute_state() -> Tuple[bool, Union[Exception, None], Union[int, None], Any]:
+
+        nonlocal api_client, project_id, zone_id
+
+        try:
+            response = api_client.get_zone(project_id, zone_id)
+            if response.zone.id != zone_id:
+                return False, ValueError("ID of zone in return not equal to ID of requested zone."), None, None
+            elif response.zone.state == _States.CreateSuccess:
+                return True, None, None, response
+            elif response.zone.state == _States.CreateFail:
+                return True, Exception("Create failed for zone with id %s" % zone_id), None, response
+            else:
+                return False, None, None, None
+        except ApiException as e:
+            return False, e, e.status, None
+        except Exception as e:
+            return False, e, None, None
+
+    wait = Wait(
+        get_zone_execute_state,
+        sleep_before_wait=sleep_before_wait,
+        throttle=throttle,
+        timeout=timeout,
+        temp_error_retry_limit=temp_error_retry_limit,
+    )
+    return wait.wait()
+
+
+def wait_for_partial_update_zone(
+    api_client: DefaultApi,
+    project_id: str,
+    zone_id: str,
+    sleep_before_wait: int = 0,
+    throttle: int = 5,
+    timeout: int = 30,
+    temp_error_retry_limit: int = 5,
+) -> ZoneResponse:
+
+    def get_zone_execute_state() -> Tuple[bool, Union[Exception, None], Union[int, None], Any]:
+
+        nonlocal api_client, project_id, zone_id
+
+        try:
+            response = api_client.get_zone(project_id, zone_id)
+            if response.zone.id != zone_id:
+                return False, ValueError("ID of zone in return not equal to ID of requested zone."), None, None
+            elif response.zone.state == _States.UpdateSuccess:
+                return True, None, None, response
+            elif response.zone.state == _States.UpdateFail:
+                return True, Exception("Update failed for zone with id %s" % zone_id), None, response
+            else:
+                return False, None, None, None
+        except ApiException as e:
+            return False, e, e.status, None
+        except Exception as e:
+            return False, e, None, None
+
+    wait = Wait(
+        get_zone_execute_state,
+        sleep_before_wait=sleep_before_wait,
+        throttle=throttle,
+        timeout=timeout,
+        temp_error_retry_limit=temp_error_retry_limit,
+    )
+    return wait.wait()
+
+
+def wait_for_delete_zone(
+    api_client: DefaultApi,
+    project_id: str,
+    zone_id: str,
+    sleep_before_wait: int = 0,
+    throttle: int = 5,
+    timeout: int = 30,
+    temp_error_retry_limit: int = 5,
+) -> ZoneResponse:
+
+    def get_zone_execute_state() -> Tuple[bool, Union[Exception, None], Union[int, None], Any]:
+
+        nonlocal api_client, project_id, zone_id
+
+        try:
+            response = api_client.get_zone(project_id, zone_id)
+            if response.zone.id != zone_id:
+                return False, ValueError("ID of zone in return not equal to ID of requested zone."), None, None
+            elif response.zone.state == _States.DeleteSuccess:
+                return True, None, None, response
+            elif response.zone.state == _States.DeleteFail:
+                return True, Exception("Delete failed for zone with id %s" % zone_id), None, response
+            else:
+                return False, None, None, None
+        except ApiException as e:
+            return False, e, e.status, None
+        except Exception as e:
+            return False, e, None, None
+
+    wait = Wait(
+        get_zone_execute_state,
+        sleep_before_wait=sleep_before_wait,
+        throttle=throttle,
+        timeout=timeout,
+        temp_error_retry_limit=temp_error_retry_limit,
+    )
+    return wait.wait()
+
+
+def wait_for_create_recordset(
+    api_client: DefaultApi,
+    project_id: str,
+    zone_id: str,
+    rr_set_id: str,
+    sleep_before_wait: int = 0,
+    throttle: int = 5,
+    timeout: int = 30,
+    temp_error_retry_limit: int = 5,
+) -> RecordSetResponse:
+
+    def get_rr_set_execute_state() -> Tuple[bool, Union[Exception, None], Union[int, None], Any]:
+
+        nonlocal api_client, project_id, zone_id, rr_set_id
+
+        try:
+            response = api_client.get_record_set(project_id, zone_id, rr_set_id)
+            if response.rrset.id != zone_id:
+                return False, ValueError("ID of rrset in return not equal to ID of requested rrset."), None, None
+            elif response.rrset.state == _States.CreateSuccess:
+                return True, None, None, response
+            elif response.rrset.state == _States.CreateFail:
+                return True, Exception("Create failed for rrset with id %s" % rr_set_id), None, response
+            else:
+                return False, None, None, None
+        except ApiException as e:
+            return False, e, e.status, None
+        except Exception as e:
+            return False, e, None, None
+
+    wait = Wait(
+        get_rr_set_execute_state,
+        sleep_before_wait=sleep_before_wait,
+        throttle=throttle,
+        timeout=timeout,
+        temp_error_retry_limit=temp_error_retry_limit,
+    )
+    return wait.wait()
+
+
+def wait_for_partial_update_recordset(
+    api_client: DefaultApi,
+    project_id: str,
+    zone_id: str,
+    rr_set_id: str,
+    sleep_before_wait: int = 0,
+    throttle: int = 5,
+    timeout: int = 30,
+    temp_error_retry_limit: int = 5,
+) -> RecordSetResponse:
+
+    def get_rr_set_execute_state() -> Tuple[bool, Union[Exception, None], Union[int, None], Any]:
+
+        nonlocal api_client, project_id, zone_id, rr_set_id
+
+        try:
+            response = api_client.get_record_set(project_id, zone_id, rr_set_id)
+            if response.rrset.id != zone_id:
+                return False, ValueError("ID of rrset in return not equal to ID of requested rrset."), None, None
+            elif response.rrset.state == _States.UpdateSuccess:
+                return True, None, None, response
+            elif response.rrset.state == _States.UpdateFail:
+                return True, Exception("Update failed for rrset with id %s" % rr_set_id), None, response
+            else:
+                return False, None, None, None
+        except ApiException as e:
+            return False, e, e.status, None
+        except Exception as e:
+            return False, e, None, None
+
+    wait = Wait(
+        get_rr_set_execute_state,
+        sleep_before_wait=sleep_before_wait,
+        throttle=throttle,
+        timeout=timeout,
+        temp_error_retry_limit=temp_error_retry_limit,
+    )
+    return wait.wait()
+
+
+def wait_for_delete_recordset(
+    api_client: DefaultApi,
+    project_id: str,
+    zone_id: str,
+    rr_set_id: str,
+    sleep_before_wait: int = 0,
+    throttle: int = 5,
+    timeout: int = 30,
+    temp_error_retry_limit: int = 5,
+) -> RecordSetResponse:
+
+    def get_rr_set_execute_state() -> Tuple[bool, Union[Exception, None], Union[int, None], Any]:
+
+        nonlocal api_client, project_id, zone_id, rr_set_id
+
+        try:
+            response = api_client.get_record_set(project_id, zone_id, rr_set_id)
+            if response.rrset.id != zone_id:
+                return False, ValueError("ID of rrset in return not equal to ID of requested rrset."), None, None
+            elif response.rrset.state == _States.DeleteSuccess:
+                return True, None, None, response
+            elif response.rrset.state == _States.DeleteFail:
+                return True, Exception("Delete failed for rrset with id %s" % rr_set_id), None, response
+            else:
+                return False, None, None, None
+        except ApiException as e:
+            return False, e, e.status, None
+        except Exception as e:
+            return False, e, None, None
+
+    wait = Wait(
+        get_rr_set_execute_state,
+        sleep_before_wait=sleep_before_wait,
+        throttle=throttle,
+        timeout=timeout,
+        temp_error_retry_limit=temp_error_retry_limit,
+    )
+    return wait.wait()
---
+++ services/dns/tests/test_wait.py
@@ -0,0 +1,143 @@
+import pytest
+from stackit.dns.wait import *
+from stackit.core.wait import Wait
+from stackit.dns.models.zone_response import ZoneResponse
+from stackit.dns.models.zone import Zone
+from unittest.mock import Mock
+
+
+def test_create_zone_wait_handler_fails_for_wrong_id():
+    zone_id = "zone_id"
+    project_id = "project_id"
+
+    api_response = ZoneResponse(
+        zone=Zone(
+            id="incorrect_id",
+            acl="acl",
+            creationFinished="creationFinished",
+            creationStarted="creationStarted",
+            defaultTTL=60,
+            dnsName="dnsName",
+            expireTime=60,
+            name="name",
+            negativeCache=60,
+            primaryNameServer="fqdn",
+            refreshTime=60,
+            retryTime=60,
+            serialNumber=123456,
+            state="CREATING",
+            type="primary",
+            updateFinished="updateFinished",
+            updateStarted="updateStarted",
+            visibility="public",
+        )
+    )
+
+    api_client = Mock()
+    api_client.get_zone.return_value = api_response
+
+    with pytest.raises(ValueError, match="ID of zone in return not equal to ID of requested zone."):
+        wait_for_create_zone(api_client, project_id, zone_id)
+
+
+def test_create_zone_wait_handler_retuns_response():
+    zone_id = "zone_id"
+    project_id = "project_id"
+
+    expected_api_response = ZoneResponse(
+        zone=Zone(
+            id=zone_id,
+            acl="acl",
+            creationFinished="creationFinished",
+            creationStarted="creationStarted",
+            defaultTTL=60,
+            dnsName="dnsName",
+            expireTime=60,
+            name="name",
+            negativeCache=60,
+            primaryNameServer="fqdn",
+            refreshTime=60,
+            retryTime=60,
+            serialNumber=123456,
+            state="CREATE_SUCCEEDED",
+            type="primary",
+            updateFinished="updateFinished",
+            updateStarted="updateStarted",
+            visibility="public",
+        )
+    )
+
+    api_client = Mock()
+    api_client.get_zone.return_value = expected_api_response
+
+    response = wait_for_create_zone(api_client, project_id, zone_id)
+
+    assert response == expected_api_response
+
+
+def test_create_zone_wait_handler_fails_for_failure():
+    zone_id = "zone_id"
+    project_id = "project_id"
+
+    api_response = ZoneResponse(
+        zone=Zone(
+            id=zone_id,
+            acl="acl",
+            creationFinished="creationFinished",
+            creationStarted="creationStarted",
+            defaultTTL=60,
+            dnsName="dnsName",
+            expireTime=60,
+            name="name",
+            negativeCache=60,
+            primaryNameServer="fqdn",
+            refreshTime=60,
+            retryTime=60,
+            serialNumber=123456,
+            state="CREATE_FAILED",
+            type="primary",
+            updateFinished="updateFinished",
+            updateStarted="updateStarted",
+            visibility="public",
+        )
+    )
+
+    api_client = Mock()
+    api_client.get_zone.return_value = api_response
+
+    with pytest.raises(Exception, match=f"Create failed for zone with id {zone_id}"):
+        wait_for_create_zone(api_client, project_id, zone_id)
+
+
+def test_create_zone_wait_handler_waits():
+    zone_id = "zone_id"
+    project_id = "project_id"
+
+    api_response = ZoneResponse(
+        zone=Zone(
+            id=zone_id,
+            acl="acl",
+            creationFinished="creationFinished",
+            creationStarted="creationStarted",
+            defaultTTL=60,
+            dnsName="dnsName",
+            expireTime=60,
+            name="name",
+            negativeCache=60,
+            primaryNameServer="fqdn",
+            refreshTime=60,
+            retryTime=60,
+            serialNumber=123456,
+            state="CREATING",
+            type="primary",
+            updateFinished="updateFinished",
+            updateStarted="updateStarted",
+            visibility="public",
+        )
+    )
+
+    api_client = Mock()
+    api_client.get_zone.return_value = api_response
+
+    with pytest.raises(TimeoutError, match="Wait has timed out"):
+        wait_for_create_zone(api_client, project_id, zone_id, timeout=1)
