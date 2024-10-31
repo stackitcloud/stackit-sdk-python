997f1ddea2de80cb67b764f99ce4d62aa72e9e5d
Melvin Klein <melvin.klein@stackit.cloud>
2024-10-31 15:28:35 +0000

    adjust example

--- examples/wait/wait_example.py
+++ examples/wait/wait_example.py
@@ -1,17 +1,11 @@
 import os
+from http import HTTPStatus
 
 from stackit.core.configuration import Configuration
 from stackit.core.wait import WaitConfig
 from stackit.dns.api.default_api import DefaultApi
-from stackit.dns.models.create_record_set_payload import CreateRecordSetPayload
 from stackit.dns.models.create_zone_payload import CreateZonePayload
-from stackit.dns.models.record_payload import RecordPayload
-from stackit.dns.wait import (
-    wait_for_create_recordset,
-    wait_for_create_zone,
-    wait_for_delete_recordset,
-    wait_for_delete_zone,
-)
+from stackit.dns.wait import wait_for_create_zone
 
 project_id = os.getenv("PROJECT_ID")
 
@@ -24,36 +18,35 @@ client = DefaultApi(config)
 create_zone_response = client.create_zone(
     create_zone_payload=CreateZonePayload(name="myZone", dnsName="testZone.com"), project_id=project_id
 )
-zone_id = create_zone_response.zone.id
+zone1_id = create_zone_response.zone.id
+zone2_id = client.create_zone(
+    create_zone_payload=CreateZonePayload(name="myZone2", dnsName="testZone2.com"), project_id=project_id
+).zone.id
+zone3_id = client.create_zone(
+    create_zone_payload=CreateZonePayload(name="myZone3", dnsName="testZone3.com"), project_id=project_id
+).zone.id
 
-# Wait for the zone to be fully created. You can also adjust the wait configuration parameters
-wait_for_zone_response = wait_for_create_zone(client, project_id, zone_id, WaitConfig(sleep_before_wait=5))
+# Wait for the zone to be fully created.
+wait_for_zone_response = wait_for_create_zone(client, project_id, zone1_id)
 
-# Create a DNS record in the newly created DNS zone
-create_record_response = client.create_record_set(
+# Optionally the wait configuration can be adjusted. It's possible to adjust just one of the parameters....
+wait_for_zone_response = wait_for_create_zone(client, project_id, zone2_id, WaitConfig(sleep_before_wait=5))
+
+# ... or all of them
+wait_for_zone_response = wait_for_create_zone(
+    client,
     project_id,
-    zone_id,
-    CreateRecordSetPayload(
-        name="a",
-        records=[
-            RecordPayload(content="192.168.0.1"),  # Will go nowhere
-        ],
-        type="A",
+    zone3_id,
+    WaitConfig(
+        sleep_before_wait=6,
+        throttle=10,
+        timeout=10,
+        temp_error_retry_limit=10,
+        retry_http_error_status_codes=[HTTPStatus.BAD_GATEWAY, HTTPStatus.GATEWAY_TIMEOUT],
     ),
 )
-rr_set_id = create_record_response.rrset.id
 
-# Wait for the DNS record to be fully created
-wait_for_rr_set_record_response = wait_for_create_recordset(client, project_id, zone_id, rr_set_id)
-
-# delete newly created recordset
-delete_rr_set_message = client.delete_record_set(project_id, zone_id, rr_set_id)
-
-# Wait for rr_set deletion
-wait_for_rr_set_deletion_response = wait_for_delete_recordset(client, project_id, zone_id, rr_set_id)
-
-# Delete newly created DNS zone
-delete_zone_message = client.delete_zone(project_id, zone_id)
-
-# Wait for deletion of DNS zone
-wait_for_zone_deletion_response = wait_for_delete_zone(client, project_id, zone_id)
+# Delete all of the zones again
+delete_zone_message = client.delete_zone(project_id, zone1_id)
+delete_zone_message = client.delete_zone(project_id, zone2_id)
+delete_zone_message = client.delete_zone(project_id, zone3_id)
