638b353e2ce7037657ce0aaee0f7337c029a67ae
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2025-10-29 17:48:40 +0100

    Generator: Update SDK /services/cdn (#2513)
    Co-authored-by: Ruben Hoenle <Ruben.Hoenle@stackit.cloud>

--- services/cdn/src/stackit/cdn/models/loki_log_sink_create.py
+++ services/cdn/src/stackit/cdn/models/loki_log_sink_create.py
@@ -20,21 +20,18 @@ from typing import Any, ClassVar, Dict, List, Optional, Set
 from pydantic import BaseModel, ConfigDict, Field, StrictStr
 from typing_extensions import Self
 
-from stackit.cdn.models.config_patch import ConfigPatch
+from stackit.cdn.models.loki_log_sink_credentials import LokiLogSinkCredentials
 
 
-class PatchDistributionPayload(BaseModel):
+class LokiLogSinkCreate(BaseModel):
     """
-    Defines a partial distribution. Set values
+    LokiLogSinkCreate
     """  # noqa: E501
 
-    config: Optional[ConfigPatch] = None
-    intent_id: Optional[StrictStr] = Field(
-        default=None,
-        description="While optional, it is greatly encouraged to provide an `intentId`.  This is used to deduplicate requests.   If multiple modifying requests with the same `intentId` for a given `projectId` are received, all but the first request are dropped. ",
-        alias="intentId",
-    )
-    __properties: ClassVar[List[str]] = ["config", "intentId"]
+    credentials: LokiLogSinkCredentials
+    push_url: StrictStr = Field(alias="pushUrl")
+    type: StrictStr
+    __properties: ClassVar[List[str]] = ["credentials", "pushUrl", "type"]
 
     model_config = ConfigDict(
         populate_by_name=True,
@@ -53,7 +50,7 @@ class PatchDistributionPayload(BaseModel):
 
     @classmethod
     def from_json(cls, json_str: str) -> Optional[Self]:
-        """Create an instance of PatchDistributionPayload from a JSON string"""
+        """Create an instance of LokiLogSinkCreate from a JSON string"""
         return cls.from_dict(json.loads(json_str))
 
     def to_dict(self) -> Dict[str, Any]:
@@ -73,14 +70,14 @@ class PatchDistributionPayload(BaseModel):
             exclude=excluded_fields,
             exclude_none=True,
         )
-        # override the default output from pydantic by calling `to_dict()` of config
-        if self.config:
-            _dict["config"] = self.config.to_dict()
+        # override the default output from pydantic by calling `to_dict()` of credentials
+        if self.credentials:
+            _dict["credentials"] = self.credentials.to_dict()
         return _dict
 
     @classmethod
     def from_dict(cls, obj: Optional[Dict[str, Any]]) -> Optional[Self]:
-        """Create an instance of PatchDistributionPayload from a dict"""
+        """Create an instance of LokiLogSinkCreate from a dict"""
         if obj is None:
             return None
 
@@ -89,8 +86,11 @@ class PatchDistributionPayload(BaseModel):
 
         _obj = cls.model_validate(
             {
-                "config": ConfigPatch.from_dict(obj["config"]) if obj.get("config") is not None else None,
-                "intentId": obj.get("intentId"),
+                "credentials": (
+                    LokiLogSinkCredentials.from_dict(obj["credentials"]) if obj.get("credentials") is not None else None
+                ),
+                "pushUrl": obj.get("pushUrl"),
+                "type": obj.get("type"),
             }
         )
         return _obj
