66b465252c43c26ec6ff034e487d0347bb9150c1
stackit-pipeline <142982727+stackit-pipeline@users.noreply.github.com>
2026-01-23 12:17:20 +0100

    Generate sqlserverflex (#2885)

--- services/sqlserverflex/src/stackit/sqlserverflex/models/__init__.py
+++ services/sqlserverflex/src/stackit/sqlserverflex/models/__init__.py
@@ -13,7 +13,6 @@
     Do not edit the class manually.
 """  # noqa: E501
 
-
 # import models into model package
 from stackit.sqlserverflex.models.acl import ACL
 from stackit.sqlserverflex.models.backup import Backup
